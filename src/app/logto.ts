de90709490ee73a5250f51547e0fa9308becf034
Randy <rebucasrandy1986@gmail.com>
2024-12-10 19:22:48 +0800

    implement logto

---
+++ src/app/logto.ts
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+  endpoint: "https://63avs0.logto.app/",
+  appId: "i8ibj9jrf953bxvws7san",
+  appSecret: "T48QjLcoSoRQEvGnPt4LPHkdDIstgvYy",
+  baseUrl: "http://localhost:3000", // Change to your own base URL
+  cookieSecret: "Pi9RYcmEqXTx9FlWxcZBSykoXbvHDR0c", // Auto-generated 32 digit secret
+  cookieSecure: process.env.NODE_ENV === "production",
+};
