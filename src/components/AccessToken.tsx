b98e109866e21d67627aea65c2b868ec6b3cdf6a
Randy <rebucasrandy1986@gmail.com>
2024-12-10 16:05:53 +0800

    reset

--- src/components/AccessToken.tsx
+++
@@ -1,8 +0,0 @@
-import { useSession } from "next-auth/react"
-export default function AccessToken() {
-    const { data: session } = useSession()
-    const accessToken = session?.user?.accessToken
-    return (
-        <div>Access Token: {accessToken}</div>
-    )
-}
