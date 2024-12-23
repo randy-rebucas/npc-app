c6bfec4adb5699884073635c90c41006180f243b
Randy <rebucasrandy1986@gmail.com>
2024-12-23 20:25:35 +0800

    update auth provider

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -2,24 +2,27 @@
 
 import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 function OnboardingCheck({ children }: { children: React.ReactNode }) {
     const { data: session } = useSession();
     const router = useRouter();
 
-    useEffect(() => {
-        const fetchUser = async (userId: string) => {
+    const fetchUser = useMemo(() => {
+        return async (userId: string) => {
             const response = await fetch(`/api/user/${userId}`);
             const user = await response.json();
             if (user?.onboardingStatus === "incomplete") {
                 router.push("/onboarding");
             }
         };
+    }, [router]);
+
+    useEffect(() => {
         if (session?.user?.id) {
-            fetchUser(session?.user?.id);
+            fetchUser(session.user.id);
         }
-    }, [session, router]);
+    }, [session, fetchUser]);
 
     return <>{children}</>;
 }
