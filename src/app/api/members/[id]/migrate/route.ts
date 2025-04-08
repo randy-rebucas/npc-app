0debb5e1e98cb79e730366d3b2b0df1fd3b77875
Randy <rebucasrandy1986@gmail.com>
2025-04-08 15:48:31 +0800

    update migrate functionality

--- src/app/api/members/[id]/migrate/route.ts
+++ src/app/api/members/[id]/migrate/route.ts
@@ -1,12 +1,8 @@
 import { MemberstackAdminService } from "@/utils/memberstack-admin";
 import { NextResponse } from "next/server";
-import connect from "@/lib/db";
-import User, {
-  UserOnBoardingStatus,
-  UserRole,
-  UserSubmissionStatus,
-} from "@/app/models/User";
-import UserProfile from "@/app/models/UserProfile";
+import { UserOnBoardingStatus, UserSubmissionStatus } from "@/app/models/User";
+import { createUser } from "@/app/actions/user";
+import { IUser } from "@/app/models/User";
 
 interface MemberstackCustomFields {
   "first-name": string;
@@ -34,6 +30,7 @@ interface MemberstackMember {
   profileImage?: string;
   stripeCustomerId?: string;
   metaData: Map<string, string>;
+  verified: boolean;
 }
 
 const splitString = (str: string) => {
@@ -47,31 +44,13 @@ export async function POST(
   try {
     const id = (await params).id;
     const response = await MemberstackAdminService.getMemberById(id);
-    const data = response.data as MemberstackMember;
+    const memberData = response.data as MemberstackMember;
 
-    await connect();
-
-    const existingUser = await User.findOne({ email: data.auth.email });
-
-    if (existingUser) {
-      return NextResponse.json({
-        success: false,
-        message: "User already exists",
-      });
-    }
-
-    const newUser = await User.create({
-      email: data.auth.email,
-      username: data.auth.email?.split("@")[0],
-      role: UserRole.PHYSICIAN,
-      // For social login, we don't store password
-      provider: "memberstack",
-      onBoardingStatus: UserOnBoardingStatus.INCOMPLETE,
-    });
+    console.log(memberData);
     const medicalLicenses = Array.isArray(
-      splitString(data.customFields["active-license-states"] || "")
+      splitString(memberData.customFields["active-license-states"] || "")
     )
-      ? splitString(data.customFields["active-license-states"] || "").map(
+      ? splitString(memberData.customFields["active-license-states"] || "").map(
           (state: string) => ({
             state: state,
             licenseNumber: "",
@@ -81,9 +60,9 @@ export async function POST(
       : [];
 
     const deaLicenses = Array.isArray(
-      splitString(data.customFields["active-dea-states"] || "")
+      splitString(memberData.customFields["active-dea-states"] || "")
     )
-      ? splitString(data.customFields["active-dea-states"] || "").map(
+      ? splitString(memberData.customFields["active-dea-states"] || "").map(
           (state: string) => ({
             state: state,
             licenseNumber: "",
@@ -92,59 +71,68 @@ export async function POST(
         )
       : [];
 
-    const practiceTypes = Array.isArray(splitString(data.customFields["practice-types"] || ""))
-      ? splitString(data.customFields["practice-types"] || "")
-      : "";
-
-    const additionalCertifications = Array.isArray(
-      splitString(data.customFields["additional-certification"] || "")
+    const practiceTypes = Array.isArray(
+      splitString(memberData.customFields["practice-types"] || "")
     )
-      ? splitString(data.customFields["additional-certification"] || "")
-          .map((certification: string) => ({
-            certification: certification,
-            issueDate: null,
-            expirationDate: null,
-            certificateUrl: null,
-            certificateNumber: null,
-          }))
+      ? splitString(memberData.customFields["practice-types"] || "")
       : [];
 
-    const userProfile = new UserProfile({
-      firstName: data.customFields["first-name"],
-      lastName: data.customFields["last-name"],
-      phone: data.customFields["phone"],
-      medicalLicenseStates: medicalLicenses,
-      deaLicenseStates: deaLicenses,
-      practiceTypes: practiceTypes,
-      npiNumber: data.customFields["npi"],
-      monthlyCollaborationRate: data.customFields["base-rate"],
-      additionalStateFee: data.customFields["state-fee"],
-      additionalNPFee: data.customFields["multi-np-fee"],
-      controlledSubstancesMonthlyFee: 0,
-      description: data.customFields["background"],
-      boardCertification: data.customFields["board-certification"],
-      additionalCertifications: additionalCertifications,
-      linkedinProfile: data.customFields["linkedin-url"],
-      profilePhotoPath: data.profileImage,
-      governmentIdPath: data.customFields["id-document-url"],
-      user: newUser._id,
-    });
-
-    await userProfile.save();
-    const user = await User.findOneAndUpdate(
-      { _id: newUser._id },
-      {
-        $set: {
-          onBoardingStatus: UserOnBoardingStatus.COMPLETED,
-          submissionStatus: UserSubmissionStatus.PENDING,
-          stripeAccountId: data.stripeCustomerId,
-          memberStackId: id,
+    const additionalCertifications = Array.isArray(
+      splitString(memberData.customFields["additional-certification"] || "")
+    )
+      ? splitString(
+          memberData.customFields["additional-certification"] || ""
+        ).map((certification: string) => ({
+          certification: certification,
+          issueDate: null,
+          expirationDate: null,
+          certificateUrl: null,
+          certificateNumber: null,
+        }))
+      : [];
+    const data = {
+      username: memberData.auth.email.split("@")[0],
+      primaryEmail: memberData.auth.email,
+      name:
+        memberData.customFields["first-name"] +
+        " " +
+        memberData.customFields["last-name"],
+      avatar: memberData.profileImage,
+      customData: {
+        role: "physician",
+        onboardingStatus: UserOnBoardingStatus.COMPLETED,
+        submissionStatus: memberData.verified ? UserSubmissionStatus.APPROVED : UserSubmissionStatus.PENDING,
+        canCreateListings: false,
+        profilePhotoPath: memberData.profileImage,
+        governmentIdPath: memberData.customFields["id-document-url"],
+        npiNumber: memberData.customFields["npi"],
+        medicalLicenseStates: medicalLicenses,
+        deaLicenseStates: deaLicenses,
+        practiceTypes: practiceTypes,
+        rateMatrix: {
+          monthlyCollaborationRate: 0,
+          additionalStateFee: 0,
+          additionalNPFee: 0,
+          controlledSubstancesMonthlyFee: 0,
+          controlledSubstancesPerPrescriptionFee: 0,
         },
-      }
-    );
+        backgroundCertification: memberData.customFields["background"],
+        description: memberData.customFields["background"],
+        boardCertification: memberData.customFields["board-certification"],
+        additionalCertifications: additionalCertifications,
+        linkedinProfile: memberData.customFields["linkedin-url"],
+        stripeAccountId: memberData.stripeCustomerId,
+      },
+      profile: {
+        familyName: memberData.customFields["last-name"],
+        givenName: memberData.customFields["first-name"],
+      },
+    };
+    console.log(data);
+    const user = await createUser(data as Partial<IUser>);
+    console.log(user);
 
-    return NextResponse.json({ success: true, user, userProfile });
-
+    return NextResponse.json({ success: true });
   } catch (error) {
     console.error(error);
     return NextResponse.json({
